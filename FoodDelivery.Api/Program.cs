fc425bc3fe9e89ab20b89f1db8ca453a2f4abe50
Ilya <79629242222@ya.ru>
2024-06-10 20:03:15 +0300

    global ex handling

--- FoodDelivery.Api/Program.cs
+++ FoodDelivery.Api/Program.cs
@@ -1,16 +1,24 @@
+using FoodDelivery.Api.Errors;
+using FoodDelivery.Api.Filters;
+using FoodDelivery.Api.Middleware;
 using FoodDelivery.Application;
 using FoodDelivery.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.
     AddApplication().
     AddInfrastructure(builder.Configuration);
+//builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<ProblemDetailsFactory, FoodDeliveryProblemDetailsFactory>();
 
 var app = builder.Build();
 
-
+//app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseExceptionHandler("/error");
 //app.UseHttpsRedirection();
 app.MapControllers();
 
